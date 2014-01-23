301ecd39b173e93bb12628d5a6e4ad778884bbe1
zdwolfe <wolfe.zach@gmail.com>
2014-01-23 13:41:43 -0500

    Add connect-proxy for development. Fixes #1

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
+  var sailsLocal = require('./config/local.js');
 
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
@@ -82,13 +83,40 @@ module.exports = function (grunt) {
         hostname: 'localhost',
         livereload: 35729
       },
+      proxies: [{
+        host: 'localhost',
+        context: '/api',
+        port: sailsLocal.port,
+        https: false,
+        changeOrigin: false,
+        xforward: false
+      }],
       livereload: {
         options: {
           open: false,
           base: [
             '.tmp',
             '<%= yeoman.app %>'
-          ]
+          ],
+          middleware: function (connect, options) {
+            if (!Array.isArray(options.base)) {
+              options.base = [options.base];
+            }
+
+            // Setup the proxy
+            var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+            // Serve static files.
+            options.base.forEach(function(base) {
+              middlewares.push(connect.static(base));
+            });
+
+            // Make directory browse-able.
+            var directory = options.directory || options.base[options.base.length - 1];
+            middlewares.push(connect.directory(directory));
+
+            return middlewares;
+          }
         }
       },
       test: {
@@ -327,6 +355,7 @@ module.exports = function (grunt) {
       'bower-install',
       'concurrent:server',
       'autoprefixer',
+      'configureProxies:server',
       'connect:livereload',
       'watch'
     ]);
