bb5b467a21f021790663de128256afb008b34cac
Zach Wolfe <wolfe.zach@gmail.com>
2014-02-10 22:33:01 -0500

    Ignore 'bootstrap.css' from grunt-bower-install tasks. This is done to fix #7

--- Gruntfile.js
+++ Gruntfile.js
@@ -215,7 +215,8 @@ module.exports = function (grunt) {
     'bower-install': {
       app: {
         html: '<%= yeoman.app %>/index.html',
-        ignorePath: '<%= yeoman.app %>/'
+        ignorePath: '<%= yeoman.app %>/',
+        exclude: ['<%= yeoman.app %>/bower_components/bootstrap/dist/css/bootstrap.css']
       }
     },
 
